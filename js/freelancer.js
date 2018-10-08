fdadbc4a5ec9f754394407682c9e84d5ffe28313
Nicholas Lee <leenick001@yahoo.com>
2018-10-08 23:07:56 +0100

    Add files via upload

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,4 +1,10 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
 
+// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
@@ -28,11 +34,4 @@ $('body').scrollspy({
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
-});
-
-$(function() {
- $('.certificate').click(function(){
-   $("#bg").attr('src',"img/certificate.jpg");
-   return false;
- });
 });
